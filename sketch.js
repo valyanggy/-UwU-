cc26a874c52092b85a0a1ba255f3954cb7b18083
Val Yang <yangx927@newschool.edu>
2021-10-18 13:25:47 -0400

    primary functions(
    next thing: try to learn to incorporate font variable stuff

--- sketch.js
+++ sketch.js
@@ -29,78 +29,39 @@
  */
 'use strict';
 
-var count = 0;
-var tileCountX = 1;
-var tileCountY = 1;
-var tileWidth = 0;
-var tileHeight = 0;
-
-var colorStep = 15;
-
-var circleCount = 0;
-var endSize = 0;
-var endOffset = 0;
-
-var actRandomSeed = 0;
-
-
+var font = 'ruddy, sans-serif';
+var letter = 'weeee';
 
 function setup() {
-    createCanvas(1000, 1000);
-    noCursor();
+    createCanvas(windowWidth, windowHeight);
+    background(255);
+    fill(0);
+
+    textFont(font);
+    textAlign(CENTER, CENTER);
 
     colorMode(HSB, 360, 100, 100);
-    rectMode(CENTER);
-    noStroke();
-    tileWidth = width / tileCountX;
-    tileHeight = height / tileCountY;
-    noFill();
-    stroke(0, 128);
 }
 
-function draw() {
-    background(mouseY / 2, 100, 100);
+function mouseMoved() {
+    clear();
+    textSize((mouseX - width / 2) * 5 + 1);
+    text(letter, mouseX, mouseY);
     fill(360 - mouseY / 2, 100, 100);
-    // ellipse(360, 360, mouseX + 4, mouseX + 4);
-
-    randomSeed(actRandomSeed);
-
-    translate(tileWidth / 2, tileHeight / 2);
-
-    circleCount = mouseX / 50 + 1;
-    endSize = map(mouseX, 0, max(width, mouseX), tileWidth / 2, 0);
-    endOffset = map(mouseY, 0, max(height, mouseY), 0, (tileWidth - endSize) / 2);
-    //  
-
-    for (var gridY = 0; gridY <= tileCountY; gridY++) {
-        for (var gridX = 0; gridX <= tileCountX; gridX++) {
-            push();
-            translate(tileWidth * gridX, tileHeight * gridY);
-            scale(1, tileHeight / tileWidth);
-
-            // var toggle = int(random(0, 4));
-            // if (toggle == 0) rotate(-HALF_PI);
-            // if (toggle == 1) rotate(0);
-            // if (toggle == 2) rotate(HALF_PI);
-            // if (toggle == 3) rotate(PI);
-
-            // draw module
-            for (var i = 0; i < circleCount; i++) {
-                var diameter = map(i, 0, circleCount, tileWidth, endSize);
-                var offset = map(i, 0, circleCount, 0, endOffset);
-                ellipse(offset, 0, diameter, diameter);
-            }
-            pop();
-        }
-    }
 
 
 }
 
-function mousePressed() {
-    actRandomSeed = random(100000);
+function mouseDragged() {
+    textSize((mouseX - width / 2) * 5 + 1);
+    text(letter, mouseX, mouseY);
+    fill(360 - mouseY / 2, 100, 100);
 }
 
-function keyPressed() {
-    if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+function keyReleased() {
+    if (keyCode == CONTROL) saveCanvas(gd.timestamp(), 'png');
+}
+
+function keyTyped() {
+    letter = key;
 }
