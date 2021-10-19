d9211b513f8f39e693b779a249dc0c75ec8c5600
Val Yang <yangx927@newschool.edu>
2021-10-18 22:27:25 -0400

    pseudo-variable

--- sketch.js
+++ sketch.js
@@ -28,40 +28,113 @@
  * s                   : save png
  */
 'use strict';
+//load fonts
+let font1;
+let font2;
+let font3;
+let font4;
+let font5;
+let font6;
+let font7;
+let font8;
+let font9;
 
-var font = 'ruddy, sans-serif';
+function preload() {
+    font1 = loadFont('fonts/Poppins-Thin.ttf');
+    font2 = loadFont('fonts/Poppins-ExtraLight.ttf');
+    font3 = loadFont('fonts/Poppins-Light.ttf');
+    font4 = loadFont('fonts/Poppins-Regular.ttf');
+    font5 = loadFont('fonts/Poppins-Medium.ttf');
+    font6 = loadFont('fonts/Poppins-SemiBold.ttf');
+    font7 = loadFont('fonts/Poppins-Bold.ttf');
+    font8 = loadFont('fonts/Poppins-ExtraBold.ttf');
+    font9 = loadFont('fonts/Poppins-Black.ttf');
+}
+
+//text
 var letter = 'weeee';
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(255);
     fill(0);
 
-    textFont(font);
+    textFont(font4);
     textAlign(CENTER, CENTER);
 
     colorMode(HSB, 360, 100, 100);
+
 }
 
 function mouseMoved() {
     clear();
-    textSize((mouseX - width / 2) * 5 + 1);
+    textSize((mouseX - width / 2) * 3 + 1);
     text(letter, mouseX, mouseY);
-    fill(360 - mouseY / 2, 100, 100);
-
-
+    // fill(360 - mouseY / 2, 100, 100);
 }
 
 function mouseDragged() {
-    textSize((mouseX - width / 2) * 5 + 1);
+
+    textSize((mouseX - width / 2) * 3 + 1);
     text(letter, mouseX, mouseY);
     fill(360 - mouseY / 2, 100, 100);
+
 }
 
 function keyReleased() {
     if (keyCode == CONTROL) saveCanvas(gd.timestamp(), 'png');
+    if (key == '1') textFont(font1);
+    if (key == '2') textFont(font2);
+    if (key == '3') textFont(font3);
+    if (key == '4') textFont(font4);
+    if (key == '5') textFont(font5);
+    if (key == '6') textFont(font6);
+    if (key == '7') textFont(font7);
+    if (key == '8') textFont(font8);
+    if (key == '9') textFont(font9);
+
+
 }
 
-function keyTyped() {
-    letter = key;
-}
+// function keyTyped() {
+//     letter = key;
+// }
+
+
+// window.addEventListener("scroll", function(e) {
+//     const maxGravity = 1000;
+//     const minGravity = 0;
+//     const posTop = 0;
+//     const posBottom = 300;
+
+
+
+
+
+//     var scrollPosition = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+//     console.log(scrollPosition);
+//     const percent = scrollPosition / 0.99;
+//     const gravityScale = percent * maxGravity - minGravity;
+//     const positionScale = percent * posBottom + posTop;
+
+//     const newGravityValue =
+//         scrollPosition > 0.99 ?
+//         maxGravity :
+//         gravityScale;
+
+
+//     if (scrollPosition >= 0.99) {
+//         text.style.setProperty("--gravity", maxGravity);
+//         // text.style.setProperty("--pos", posBottom + '%');
+//     } else {
+//         text.style.setProperty("--gravity", newGravityValue);
+//         // text.style.setProperty("--pos", positionScale + '%');
+//     }
+
+
+
+
+
+
+// });
